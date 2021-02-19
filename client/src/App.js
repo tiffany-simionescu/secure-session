cec6bb4692bd4624cce85c9e18cbee3289be278f
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-02-19 13:07:25 -0500

    React App, passport

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Home from './Home';
+import Profile from './Profile';
+import Loading from './Loading';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [auth, setAuth] = useState(null); 
+
+  // if (auth === false) {
+  if (auth === null) {
+    return <Loading />
+  }
+  if (auth) {
+    return <Profile />
+  }
+  return <Home />
 }
 
 export default App;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const session = require('cookie-session');
 const helmet = require('helmet');
 const hpp = require('hpp');
 const csurf = require('csurf');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({path: path.resolve(__dirname, '.env')});
 
 // Express app
 const app = express();
@@ -15,7 +19,7 @@ app.use(hpp());
 app.use(
   session({
     name: 'session',
-    secret: 'secretKey',
+    secret: process.env.COOKIE_SECRET,
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
   })
 );
